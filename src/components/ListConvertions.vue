8867a664992a495d41ebc3e69cd48c4f2d569176
Matjaz Cvek <matjaz.cvek@gmail.com>
2020-05-24 15:45:12 +1200

    style edit format

--- src/components/ListConvertions.vue
+++ src/components/ListConvertions.vue
@@ -2,19 +2,17 @@
   <div class="pb-2">
     <div class="pa-2" v-for="(convertion, index) in convertions" :key="index">
       <div v-if="convertion.type === 'dd'">
-        <p class="title font-weight-light">Decimal Degrees</p>
-        <p>{{ convertion.latitude }}</p>
-        <p>{{ convertion.longitude }}</p>
+        <p class="title font-weight-light">DD.dddddd° (Decimal Degrees)</p>
+        <p>{{ convertion.latitude }} {{ convertion.longitude }}</p>
         <v-divider></v-divider>
       </div>
       <div v-else-if="convertion.type === 'dm'">
-        <p class="title font-weight-light">Degrees Minutes</p>
+        <p class="title font-weight-light">H DD° MM.mmm´ (Degrees Minutes)</p>
         <p>
           {{ convertion.latitude.hemisphere }}
           {{ convertion.latitude.degrees }}
           {{ convertion.latitude.minutes }}
-        </p>
-        <p>
+
           {{ convertion.longitude.hemisphere }}
           {{ convertion.longitude.degrees }}
           {{ convertion.longitude.minutes }}
@@ -22,14 +20,15 @@
         <v-divider></v-divider>
       </div>
       <div v-else-if="convertion.type === 'dms'">
-        <p class="title font-weight-light">Degrees Minutes Seconds</p>
+        <p class="title font-weight-light">
+          H DD° MM´ SS.sss´´ (Degrees Minutes Seconds)
+        </p>
         <p>
           {{ convertion.latitude.hemisphere }}
           {{ convertion.latitude.degrees }}
           {{ convertion.latitude.minutes }}
           {{ convertion.latitude.seconds }}
-        </p>
-        <p>
+
           {{ convertion.longitude.hemisphere }}
           {{ convertion.longitude.degrees }}
           {{ convertion.longitude.minutes }}
